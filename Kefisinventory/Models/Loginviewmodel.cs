7b7bfac6fec03be0665586254390a79816ce0352
Francis Kingori <Francis.Kingori@tovutigroup.com>
2021-07-31 07:06:15 +0300

    update

---
+++ Kefisinventory/Models/Loginviewmodel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kefisinventory.Models
+{
+    public class Loginviewmodel
+    {
+        [Required(ErrorMessage = "Email Address is required!")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Enter a valid email address")]
+        [Display(Name = "Email Address")]
+        public string Emailaddress { get; set; }
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password, ErrorMessage = "Enter a valid password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ Kefisinventory/Models/UserDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kefisinventory.Models
+{
+    public class UserDataModel
+    {
+        public long Id { get; set; }
+        public long UserCode { get; set; }
+        public string ShopName { get; set; }
+    }
+}
